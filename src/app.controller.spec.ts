a6a73f9efcbdf157c69970def7423d1dbb4bbe4c
Wexengos <gnos2007@gmail.com>
2022-12-28 15:32:55 -0300

    Aulas finalizadas

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 describe('NotificationsController', () => {
   let notificationsController: NotificationsController;
 
+  // teste JEST
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [NotificationsController],
